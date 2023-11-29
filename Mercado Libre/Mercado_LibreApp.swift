dfd384323095445b65246866311d38c47006861a
Edward Narváez <john.narvaez@globant.com>
2023-11-29 14:00:51 -0500

    Initial Commit

---
+++ Mercado Libre/Mercado_LibreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mercado_LibreApp.swift
+//  Mercado Libre
+//
+//  Created by John Edward Narvaez Londono on 29/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Mercado_LibreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
